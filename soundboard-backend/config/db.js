7adf2995940007631d042b70e8de05b8c38b9bfc
Shashwat Mahendra <shashwatmahender2104@gmail.com>
2025-05-01 14:40:17 +0530

    done

--- soundboard-backend/config/db.js
+++ soundboard-backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/ProjectBoard");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error(err.message);
